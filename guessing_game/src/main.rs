8c21b8011fd75afafa9657d39cc5a588e56e6a82
Anderson Babinski <andeerlb@gmail.com>
2025-08-13 20:30:50 -0300

    rust guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,36 @@
+use std::cmp::Ordering;
+
 macro_rules! ask_for_input {
-    ($msg:expr) => {
-        {
-            use std::io; // input/output library
-            println!(">> {}", $msg);
-            let mut guess = String::new();
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Failed to read line");
-            guess
-        }
-    };
+    ($msg:expr) => {{
+        use std::io;
+        println!(">> {}", $msg);
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        guess
+    }};
 }
 
 fn main() {
-    println!("Welcome to the Guessing Game!"); // the !, means that it is a macro, not a function
-    let guess = ask_for_input!("Please enter your guess:"); // using the custom macro
+    println!("Welcome to the Guessing Game!");
+    let secret_number = rand::random_range(1..=10);
 
-    println!("Hello, world!");
-    println!("You guessed: {guess}");
+    loop {
+        let guess: i32 = ask_for_input!("Please enter your guess:")
+            .trim() // remove \n
+            .parse() // converte para i32
+            .expect("Please type a valid number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
