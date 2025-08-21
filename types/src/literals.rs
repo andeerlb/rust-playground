986839a26975da87e8630a01192b50b69846a941
Anderson Babinski <andeerlb@gmail.com>
2025-08-21 20:26:00 -0300

    literals and operators

--- types/src/literals.rs
+++ types/src/literals.rs
@@ -24,15 +24,20 @@ fn main() {
 
     // Short-circuiting boolean logic
     println!("true AND false is {}", true && false);
+    // && means "and"
     println!("true OR false is {}", true || false);
     println!("NOT true is {}", !true);
 
     // Bitwise operations
     println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    // & means "and"
     println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
     println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    // ˆ means "exclusive or"
     println!("1 << 5 is {}", 1u32 << 5);
+    // << means "shift left"
     println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+    // >> means "shift right"
 
     // Use underscores to improve readability!
     println!("One million is written as {}", 1_000_000u32);
