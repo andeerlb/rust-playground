c37f3e3cf427f98c10d79271bb3958cbdeb8ae8f
Anderson Babinski <andeerlb@gmail.com>
2025-10-15 20:29:48 -0300

    tests with question mark

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -27,6 +27,28 @@ mod tests {
     }
 }
 
+// Tests and ?
+// None of the previous unit test examples had a return type. But in Rust 2018, your unit tests can return Result<()>, which lets you use ? in them! This can make them much more concise.
+fn sqrt(number: f64) -> Result<f64, String> {
+    if number >= 0.0 {
+        Ok(number.powf(0.5))
+    } else {
+        Err("negative floats don't have square roots".to_owned())
+    }
+}
+
+#[cfg(test)]
+mod tests_with_return {
+    use super::*;
+
+    #[test]
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
