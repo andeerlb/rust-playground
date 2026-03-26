a094b92fe03eccd011c28f1d91339dec45c602a2
Anderson Babinski <andeerlb@gmail.com>
2026-03-26 20:41:13 -0300

    imuttable variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
+    // variables are immutable by default, but we can make them mutable using the `mut` keyword
     let apples_count = 50;
-    let apples = 50;
+    let mut apples = 50;
     println!("We have {} apples.", apples);
     let oranges = 14 + 6;
     let fruits = apples + oranges;
     println!("We have {} fruits in total.", fruits);
     println!("We have {} oranges.", oranges);
     println!("apples count is {}", apples_count);
+
+    apples = 40;
+    println!("Now we have {} apples.", apples);
 }
 
