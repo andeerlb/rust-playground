11f2a5c00a320d520db3f2e688b75e3c5f9cbc20
Anderson Babinski <andeerlb@gmail.com>
2026-03-26 20:35:12 -0300

    rust interpolation

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let apples_count = 50;
+    let apples = 50;
+    println!("We have {} apples.", apples);
+    let oranges = 14 + 6;
+    let fruits = apples + oranges;
+    println!("We have {} fruits in total.", fruits);
+    println!("We have {} oranges.", oranges);
+    println!("apples count is {}", apples_count);
+}
+
